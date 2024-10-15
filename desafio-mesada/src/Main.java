da23360d8a10d067d0f61cf79e2f5c315316ebfd
qu1nze <lucas.rosseto.r@gmail.com>
2024-10-15 10:46:56 -0300

    Desafio mesada

---
+++ desafio-mesada/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int mesada = 50;
+
+        System.out.println("Digite a quantidade de meses que polpou o dinheiro: ");
+        int entrada = leitor.nextInt();
+
+        System.out.println("Em " + entrada + " meses voce tera: " + entrada * mesada + " reais");
+    }
+}
